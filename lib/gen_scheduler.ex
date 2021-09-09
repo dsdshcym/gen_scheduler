f864fe0d1286c3cd9f50150fe9c9f1bd48e9e5f5
Yiming Chen <dsdshcym@gmail.com>
2021-09-09 14:15:22 +0800

    mix new gen_scheduler

---
+++ lib/gen_scheduler.ex
@@ -0,0 +1,18 @@
+defmodule GenScheduler do
+  @moduledoc """
+  Documentation for `GenScheduler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenScheduler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gen_scheduler_test.exs
@@ -0,0 +1,8 @@
+defmodule GenSchedulerTest do
+  use ExUnit.Case
+  doctest GenScheduler
+
+  test "greets the world" do
+    assert GenScheduler.hello() == :world
+  end
+end
