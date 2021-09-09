d50a8ca52af77f4ea45bd1a8acdd837879b388e1
Yiming Chen <dsdshcym@gmail.com>
2021-09-09 14:16:17 +0800

    remove unused GenScheduler.hello/0

--- lib/gen_scheduler.ex
+++ lib/gen_scheduler.ex
@@ -2,17 +2,4 @@ defmodule GenScheduler do
   @moduledoc """
   Documentation for `GenScheduler`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GenScheduler.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/gen_scheduler_test.exs
+++ test/gen_scheduler_test.exs
@@ -1,8 +1,4 @@
 defmodule GenSchedulerTest do
   use ExUnit.Case
   doctest GenScheduler
-
-  test "greets the world" do
-    assert GenScheduler.hello() == :world
-  end
 end
